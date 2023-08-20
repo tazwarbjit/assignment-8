2d92141417204460b75b7858469bff6667364a10
Tazwar Mohammed <tazwarmohammed1998@gmail.com>
2023-08-20 23:19:50 +0600

    Implement feature for adding employee

--- src/main/java/bjit/academy/Main.java
+++ src/main/java/bjit/academy/Main.java
@@ -6,6 +6,15 @@ public class Main {
     public static void main(String args[]) throws SQLException {
         DBOperations dbObj = new DBOperations();
         dbObj.doConnectDB();
+
+        System.out.println("Existing default employees");
+        dbObj.fetchEmployeeData();
+
+        System.out.println("=============================================");
+
+        System.out.println("Employees after adding");
+        Employee newEmployee = new Employee("Mahnoor", "Marium", "Banani", 90000, 1, 1);
+        dbObj.addEmployee(newEmployee);
         dbObj.fetchEmployeeData();
     }
 }
